0a78c87ecae94fbaf4ae5a348854cd1d1f5b0c17
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-06-06 19:41:00 +0100

    add new column species

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-vet_clinic(#    id integer PRIMARY KEY NOT NULL,
-vet_clinic(#     name varchar(100),
-vet_clinic(#     date_of_birth date,
-vet_clinic(#     escape_attempts integer,
-vet_clinic(#     neutered boolean,
-vet_clinic(#     weight_kg decimal,
-vet_clinic(#     PRIMARY KEY (ID)
-vet_clinic(# );
+    id integer PRIMARY KEY NOT NULL,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal);
+
+    ALTER TABLE animals
+vet_clinic-# ADD COLUMN species varchar(100);
