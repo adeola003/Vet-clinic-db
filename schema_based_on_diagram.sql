d77b6be534999651f749fe18faae914d58ed05d5
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-06-13 21:00:06 +0100

    create new database and create tables as per diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,38 @@
+--Tables
+CREATE DATABASE new_clinic;
+
+CREATE TABLE patients(
+	id INT NOT NULL PRIMARY KEY,
+	name VARCHAR UNIQUE NOT NULL, 
+	date_of_birth DATE
+);
+
+CREATE TABLE medical_histories(
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	addimited_at TIMESTAMP, 
+	patient_id BIGINT REFERENCES patients (id),
+    status VARCHAR
+);
+
+CREATE TABLE treatments (
+    id INT NOT NULL PRIMARY KEY,
+    type VARCHAR,
+    name VARCHAR
+);
+
+CREATE TABLE invoices (
+    id INT NOT NULL PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history__id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+    id INT NOT NULL PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id),
+);
